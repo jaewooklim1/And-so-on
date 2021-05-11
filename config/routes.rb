4d1b898fc7226de4d89e1c9aff09c8e250b89175
jaewooklim1 <jaewooklim1@gmail.com>
2021-05-11 17:05:28 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resource :users, only: [:create]
+    resource :session, only: [:create, :destroy]
+  end
+
 end
