36c10a7734a9113d12eaf1376634741c85c02755
jaewooklim1 <jaewooklim1@gmail.com>
2021-05-12 02:10:31 -0400

    got login and signup

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,13 +3,16 @@ import { Route } from "react-router";
 import { AuthRoute } from "../utils/route_util";
 import login_form_container from "./login_form_container";
 import signup_form_container from "./signup_form_container";
+import GreetingContainer from "./greeting/greeting_container";
 
 const App = () => (
   <div>
+    <header>
     <h1>And-so-on</h1>
-    <Route path="/login" component={login_form_container} />
-    <Route path="/signup" component={signup_form_container} />
-       {/* <GreetingContainer /> */}
+    {/* <Route path="/login" component={login_form_container} /> */}
+    {/* <Route path="/signup" component={signup_form_container} /> */}
+    <GreetingContainer />
+    </header>
   </div>
 );
 
--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,13 +1,13 @@
 import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from "../actions/session_actions";
 
-const sessionErrorsReducer = (state = [], action) => {
+const sessionErrorsReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_ERRORS:
-      debugger
+      // debugger
       return action.errors;
     case RECEIVE_CURRENT_USER:
-      return [];
+      return {};
     default:
       return state;
   }
