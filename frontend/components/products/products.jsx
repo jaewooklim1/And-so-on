f757bfc4d8831259d9290601da5a13f146562e51
jaewooklim1 <jaewooklim1@gmail.com>
2021-05-21 09:23:23 -0400

    more stuff

--- frontend/components/products/products.jsx
+++ frontend/components/products/products.jsx
@@ -34,18 +34,14 @@ class Product extends React.Component {
                     </div>
                     <div className='product-properties'>
                         <label className='price'>Price:  
-                            <span className='price-num'>  ${Price}<span className='free-ship'> & FREE Shipping.</span></span>
+                            <span className="Price and Shipping">  ${Price} & FREE Shipping.</span>
                         </label>
-                        <div className='show-col1'>
-                            <label className='product-show-col1'>Artist:<span>  {product.artist}</span></label>
+                        <div className='product-show-container'>                         
+                            <label className='product-show'>Year:<span>  {product.year}</span></label>
                             <br></br>
-                            <label className='product-show-col1'>Year:<span>  {product.year}</span></label>
-                            <br></br>
-                            <label className='product-show-col1'> Style:<span>  {product.style}</span></label>
-                            <br></br>
-                            <label className='product-show-col1'>Type:<span>  {product.media_type}</span></label>
-                            <br></br>
-                            <label className='product-show-col1' id='about'>About this product
+                            <label className='product-show'>Category:<span>  {product.style}</span></label>
+                            <br></br>             
+                            <label className='product-show' id='about'>About this product
                                 <br></br>
                                 <span id='description'>{product.description}</span>
                                 </label>
